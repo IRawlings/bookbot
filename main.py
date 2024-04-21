0a2d909ebfd0d47296e513c4caac0b61a9b098e4
Iain Rawlings <Iain.Rawlings@gmail.com>
2024-04-21 12:21:56 +0100

    plan new char report function

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ def main():
     text = get_book_text(book_path)
     word_count = get_book_words(text)
     letter_count = get_char_count(text)
+    s = sort_on(letter_count)
+    
     print(text)
     print(word_count)
     print(letter_count)
-
+    print(s)
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,22 +25,24 @@ def get_char_count(book):
     processed = book.lower()
     # look at every character of lowered text
     for char in processed:
-        # check character .isalpha().
-        if char.isalpha():
-            # if yes: does it already exist in the dict?
-            if char in letters:
+        if char in letters:
                 # Yes: add 1
-                letters[char] += 1
-            else:
+            letters[char] += 1
+        else:
                 # No: add to dict with value of 1
                 # letters[char] = 1 also works here, however [char] will not work with update, hence the f string
-                letters.update({f"{char}": 1})
+            letters.update({f"{char}": 1})
+            # letters[char] = 1 # also works
+    # return dict
+    # don't forget to print returned result in main (following previous logic, but you could print it in the function)
     return letters
             
+def sort_on(dict):
+    return dict[0]
+
               
               
-        # return dict
-        # don't forget to print returned result in main (following previous logic, but you could print it in the function)
+       
 
 
 if __name__ == '__main__':
